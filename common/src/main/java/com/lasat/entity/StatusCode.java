afdea0fa07c0fa272f14e6e067f33378ad3d18ca
cuikunxu <961661187@qq.com>
2022-05-23 19:23:22 +0800

    Models are created. The eureka server is also set.

---
+++ common/src/main/java/com/lasat/entity/StatusCode.java
@@ -0,0 +1,10 @@
+package com.lasat.entity;
+
+public class StatusCode {
+    public static final int OK = 20000;//operation succeed
+    public static final int ERROR = 20001;//operation failed
+    public static final int LOGIN_ERROR = 20002;
+    public static final int ACCESS_ERROR = 20003;
+    public static final int REMOTE_ERROR = 20004;
+    public static final int REP_ERROR = 20005;
+}
---
+++ model-sample/src/main/java/com/lasat/model/sample/controller/SamplePointController.java
@@ -0,0 +1,30 @@
+package com.lasat.model.sample.controller;
+
+import com.lasat.entity.Result;
+import com.lasat.entity.StatusCode;
+import com.lasat.model.sample.bean.SamplePoint;
+import com.lasat.model.sample.service.SamplePointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/samplePoint")
+public class SamplePointController {
+
+    @Autowired
+    private SamplePointService samplePointService;
+
+    @PutMapping
+    public Result<String> insert(@RequestBody SamplePoint samplePoint) {
+        samplePointService.insert(samplePoint);
+        return new Result<>(true, StatusCode.OK, "insert succeed");
+    }
+
+    @GetMapping("/{startIndex}")
+    public Result<List<SamplePoint>> getByPartitions(@PathVariable Integer startIndex) {
+        List<SamplePoint> samplePoints = samplePointService.selectPartitions(startIndex);
+        return new Result<>(true, StatusCode.OK, "get points succeed",samplePoints);
+    }
+}
